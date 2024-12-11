7b139472410377ebe527498540e80c2a40de18c6
Georgeta Gorgan <159003750+Georgiana0403@users.noreply.github.com>
2024-12-11 14:00:56 +0100

    Update outputs.tf

--- ecs_cluster/outputs.tf
+++ ecs_cluster/outputs.tf
@@ -1,3 +1,25 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.main_vpc.id
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public_subnets[*].id
+}
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = aws_subnet.private_subnets[*].id
+}
+
+output "security_group_id" {
+  description = "The ID of the ECS security group"
+  value       = aws_security_group.ecs_sg.id
+}
+output "ecs_task_execution_role_arn" {
+  value = aws_iam_role.ecs_task_execution_role.arn
+}
 output "aws_cloudwatch_log_group" {
   value = [for log_group in aws_cloudwatch_log_group.ecs_cw_log_group:  log_group.name]
 }
