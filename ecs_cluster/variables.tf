3434d78338b7ff4056a5a2b1d09be1a54b59cc18
Georgeta Gorgan <159003750+Georgiana0403@users.noreply.github.com>
2024-12-11 14:02:22 +0100

    Update variables.tf

--- ecs_cluster/variables.tf
+++ ecs_cluster/variables.tf
@@ -2,6 +2,29 @@ variable "app_name" {
   description = "Application Name"
   type        = string
 }
+variable "region" {
+  description = "AWS Region"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.10.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.10.1.0/24", "10.10.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.10.101.0/24", "10.10.102.0/24"]
+}
 variable "ecs_role_arn" {
   description = "IAM Role for ECS"
   type        = string
@@ -45,4 +68,29 @@ variable "target_group_arns" {
   type = map(object({
     arn       = string
   }))
+}
+# New variables for VPC configuration
+variable "enable_dns_hostnames" {
+  description = "Enable DNS hostnames in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_support" {
+  description = "Enable DNS support in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "az_count" {
+  description = "Number of Availability Zones to use"
+  type        = number
+  default     = 2
+}
+
+# Optional tagging variables
+variable "tags" {
+  description = "Additional tags to apply to resources"
+  type        = map(string)
+  default     = {}
 }
